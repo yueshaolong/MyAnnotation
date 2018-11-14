3b1fd18453f1f9eef5135f07a3c904f8a0b67338
yueshaolong <yueshaolong@vsxin.com>
2018-11-14 14:34:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myannotation/ActivityWithExtra.java
@@ -0,0 +1,43 @@
+package com.example.myannotation;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.Date;
+
+@EActivity(R.layout.activity_with_extra)
+public class ActivityWithExtra extends Activity {
+
+    public static final String MY_STRING_EXTRA = "myStringExtra";
+    public static final String MY_DATE_EXTRA = "myDateExtra";
+    public static final String MY_INT_EXTRA = "myIntExtra";
+
+    @ViewById
+    TextView extraTextView;
+
+    @Extra(MY_STRING_EXTRA)
+    String myMessage;
+
+    @Extra(MY_DATE_EXTRA)
+    Date myDate;
+
+    @Extra("unboundExtra")
+    String unboundExtra = "unboundExtraDefaultValue";
+
+    /**
+     * The logs will output a classcast exception, but the program flow won't be interrupted
+     */
+    @Extra(MY_INT_EXTRA)
+    String classCastExceptionExtra = "classCastExceptionExtraDefaultValue";
+
+    @AfterViews
+    protected void init() {
+        extraTextView.setText(myMessage + " " + myDate + " " + unboundExtra + " " + classCastExceptionExtra);
+    }
+
+}
